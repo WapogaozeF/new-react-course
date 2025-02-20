b4ee16d9c41af0e586d32ebd76dee3b49cbe300e
Verner Ravil <werner.ravil@gmail.com>
2025-02-20 16:13:04 +0500

    Added CounterHistory component to App

--- optimization/src/App.jsx
+++ optimization/src/App.jsx
@@ -1,28 +1,29 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Counter from './components/Counter/Counter.jsx';
-import Header from './components/Header.jsx';
-import { log } from './log.js';
-import ConfigureCounter from './components/Counter/ConfigureCounter.jsx';
+import Counter from "./components/Counter/Counter.jsx";
+import Header from "./components/Header.jsx";
+import { log } from "./log.js";
+import ConfigureCounter from "./components/Counter/ConfigureCounter.jsx";
 
 function App() {
-  log('<App /> rendered');
+	log("<App /> rendered");
 
 	const [chosenCount, setChosenCount] = useState(0);
- 
-  function handleSetCount(nweCount) {
-    setChosenCount(nweCount);
-  }
 
-  return (
-    <>
-      <Header />
-      <main>
-        <ConfigureCounter onSet={handleSetCount} />
-        <Counter initialCount={chosenCount} />
-      </main>
-    </>
-  );
+	function handleSetCount(nweCount) {
+		setChosenCount(nweCount);
+	}
+
+	return (
+		<>
+			<Header />
+			<main>
+				<ConfigureCounter onSet={handleSetCount} />
+				<Counter key={chosenCount} initialCount={chosenCount} />
+				<Counter initialCount={0} />
+			</main>
+		</>
+	);
 }
 
 export default App;
