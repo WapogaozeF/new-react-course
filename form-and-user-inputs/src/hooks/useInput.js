e965f041db820c753fc16aeed5d47957516c9935
Verner Ravil <werner.ravil@gmail.com>
2025-02-24 01:04:08 +0500

    Added useInput to StateLogin

--- form-and-user-inputs/src/hooks/useInput.js
+++ form-and-user-inputs/src/hooks/useInput.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 
-export function useInput(defaultValue) {
+export function useInput(defaultValue, validationFn) {
 	const [enteredValue, setEnteredValue] = useState(defaultValue);
 	const [didEdit, setDidEdit] = useState(false);
+
+	const valueIsValid = validationFn(enteredValue);
 
 	function handleInputChange(event) {
 		setEnteredValue(event.target.value);
@@ -17,5 +19,6 @@ export function useInput(defaultValue) {
 		value: enteredValue,
 		handleInputChange,
 		handleInputBlur,
+		hasError: didEdit && !valueIsValid,
 	};
 }
