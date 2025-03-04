9fc62299e570e0c5d801e5f6737ffc5a0a2f3492
Verner Ravil <werner.ravil@gmail.com>
2025-03-04 17:51:28 +0500

    Added Redux toolkit

--- diving-into-redux/src/store/index.js
+++ diving-into-redux/src/store/index.js
@@ -1,15 +1,31 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case "increment":
 			return {
+				...state,
 				counter: state.counter + 1,
+			};
+
+		case "increase":
+			return {
+				...state,
+				counter: state.counter + action.amount,
 			};
 
 		case "decrement":
 			return {
+				...state,
 				counter: state.counter - 1,
+			};
+
+		case "toggle":
+			return {
+				...state,
+				showCounter: !state.showCounter,
 			};
 
 		default:
