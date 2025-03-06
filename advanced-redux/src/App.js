26c0b379a1f357c4a77b9eb95c8a7c3f5c4cd786
Verner Ravil <werner.ravil@gmail.com>
2025-03-07 00:01:05 +0500

    Init Advanced Redux section

---
+++ advanced-redux/src/App.js
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+
+function App() {
+	const showCart = useSelector((state) => state.ui.cartIsVisible);
+	const cart = useSelector((state) => state.cart);
+
+	useEffect(() => {
+		fetch(
+			"https://react-redux-course-a9e12-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+			{
+				method: "PUT",
+				body: JSON.stringify(cart),
+			},
+		);
+	}, [cart]);
+
+	return (
+		<Layout>
+			{showCart && <Cart />}
+			<Products />
+		</Layout>
+	);
+}
+
+export default App;
