2c0d3a20460ae6d94ef1cd40c1c6132b5515ae80
Verner Ravil <werner.ravil@gmail.com>
2025-03-07 22:32:38 +0500

    Added using dynamic router

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetail";
 
 // const routeDefinitions = createRoutesFromElements(
 // 	<Route>
@@ -23,6 +24,7 @@ const router = createBrowserRouter([
 				path: "/products",
 				element: <ProductsPage />,
 			},
+			{ path: "/products/:productId", element: <ProductDetailPage /> },
 		],
 	},
 ]);
