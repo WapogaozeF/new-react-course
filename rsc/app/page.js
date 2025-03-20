872b2c37d8f409cfac8b4349bbfb3168846f72b1
Verner Ravil <werner.ravil@gmail.com>
2025-03-20 15:34:28 +0500

    Fixed Next.js project Home page

--- rsc/app/page.js
+++ rsc/app/page.js
@@ -6,17 +6,18 @@ import UsePromiseDemo from "@/components/UsePromisesDemo";
 import ErrorBoundary from "@/components/ErrorBoundary";
 
 export default function Home() {
-	const fetchUsersPromise = new Promise((resolve) =>
+	const fetchUsersPromise = new Promise((resolve, reject) =>
 		setTimeout(async () => {
 			const data = await fs.readFile("dummy-db.json", "utf-8");
 			const users = JSON.parse(data);
 			resolve(users);
+			// reject(new Error("Error!"));
 		}, 2000),
 	);
 
 	return (
 		<main>
-			<ErrorBoundary>
+			<ErrorBoundary fallback={<p>Something went wrong!</p>}>
 				<Suspense fallback={<p>Loading users...</p>}>
 					<UsePromiseDemo usersPromise={fetchUsersPromise} />
 				</Suspense>
