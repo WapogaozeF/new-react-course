b2a601fe523052060a66aea3d54a33d237048ba8
Verner Ravil <werner.ravil@gmail.com>
2025-03-20 15:27:59 +0500

    Loading data by use hook

--- rsc/app/page.js
+++ rsc/app/page.js
@@ -1,11 +1,22 @@
-import UsePromiseDemo from "@/components/UsePromisesDemo";
+import fs from "node:fs/promises";
+
 import { Suspense } from "react";
 
-export default async function Home() {
+import UsePromiseDemo from "@/components/UsePromisesDemo";
+
+export default function Home() {
+	const fetchUsersPromise = new Promise((resolve) =>
+		setTimeout(async () => {
+			const data = await fs.readFile("dummy-db.json", "utf-8");
+			const users = JSON.parse(data);
+			resolve(users);
+		}, 2000),
+	);
+
 	return (
 		<main>
 			<Suspense fallback={<p>Loading users...</p>}>
-				<UsePromiseDemo />
+				<UsePromiseDemo usersPromise={fetchUsersPromise} />
 			</Suspense>
 		</main>
 	);
