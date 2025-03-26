f644257e883dd7c9082afbcad1a23e7088fe5ed7
Verner Ravil <werner.ravil@gmail.com>
2025-03-26 15:16:48 +0500

    Added Compound Component Accordion

--- best-practices/src/App.jsx
+++ best-practices/src/App.jsx
@@ -1,5 +1,44 @@
+import Accordion from "./components/Accordion/Accordion";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+	return (
+		<main>
+			<section>
+				<h2>Why work with us?</h2>
+
+				<Accordion className="accordion">
+					<Accordion.Item className="accordion-item" id="experience">
+						<Accordion.Title className="accordion-item-title">
+							We got 20 year of experience
+						</Accordion.Title>
+						<Accordion.Content className="accordion-item-content">
+							<article>
+								<p>You can&apost;t go ground with us.</p>
+								<p>
+									We are in the business of planning highly individualize
+									vacation trips for more than 20 years.
+								</p>
+							</article>
+						</Accordion.Content>
+					</Accordion.Item>
+					<Accordion.Item className="accordion-item" id="local-guides">
+						<Accordion.Title className="accordion-item-title">
+							We're working with local guides
+						</Accordion.Title>
+						<Accordion.Content className="accordion-item-content">
+							<article>
+								<p>We are not doing this along from our office .</p>
+								<p>
+									Instead, we are working with local guides to ensure a safe and
+									pleasant vacation.
+								</p>
+							</article>
+						</Accordion.Content>
+					</Accordion.Item>
+				</Accordion>
+			</section>
+		</main>
+	);
 }
 
 export default App;
