aa1966827280214a0f07a5752a8cd47e306493a0
Verner Ravil <werner.ravil@gmail.com>
2025-03-30 21:28:22 +0500

    Added Counter store

--- extra-course/zustand-ts/src/App.tsx
+++ extra-course/zustand-ts/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCounterStore } from "./store";
+
+import classes from "./App.module.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const count = useCounterStore((state) => state.count);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return <OtherComponent count={count} />;
 }
 
-export default App
+const OtherComponent = ({ count }: { count: number }) => {
+	const incrementAsync = useCounterStore((state) => state.incrementAsync);
+	const decrement = useCounterStore((state) => state.decrement);
+
+	return (
+		<div className={classes.count}>
+			{count}
+			<div>
+				<button onClick={incrementAsync}>Increment Async</button>
+				<button onClick={decrement}>Decrement</button>
+			</div>
+		</div>
+	);
+};
+
+export default App;
