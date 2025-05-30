24ed32d3742c3917ab58d7b7c66195b0c5f2c385
Verner Ravil <werner.ravil@gmail.com>
2025-05-30 16:13:31 +0500

    Fixed import in main.tsx

--- extra-course/tanstack-router-practice/src/main.tsx
+++ extra-course/tanstack-router-practice/src/main.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
-import ErrorComponent from "./components/ErrorComponent";
 
 import "./index.css";
 
